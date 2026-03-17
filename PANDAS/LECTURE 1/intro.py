3706c6a56e898fe4e590e0d7bc4f5dd6c0ac9ba1
Shresth Sinha <aashresth.sinha@gmail.com>
2026-03-18 03:22:52 +0530

    REVISON PART 1

--- PANDAS/LECTURE 1/intro.py
+++ PANDAS/LECTURE 1/intro.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
-# pd.set_option('display.max_rows',5)
-# pd.set_option('display.max_columns',5)
+# df=pd.read_csv("../survey_results_public.csv") #To read the csv file correctly.
+# print(df)  #To print the directory
 
-df=pd.read_csv('../survey_results_public.csv')
-# print(df)
-# print(df.head())
-# print(df.tail())
-# print(df.shape)
-# print(df.info())
+# print(df.shape)  #To see the shape rows*columns
+# print(df.info()) #To see info specially data types of column..
 
-# schema_df=pd.read_csv('../survey_results_schema.csv')
-# print(schema_df)
-# print(schema_df.info())
+# pd.set_option("display.max_columns",5)   #To set that truncation in column display happens if columns are more than 5
+# pd.set_option("display.max_rows",5)      #To set that truncation in row display happens if rows are more than 5
+
+# print(df.head())   #To print x number of data sets/rows from top
+# print()
+# print()
+# print(df.tail())   #To print x number of data sets/rows from bottom
+#By default the value if x is 5.
--- PANDAS/LECTURE 2/panaspyt.py
+++ PANDAS/LECTURE 2/panaspyt.py
@@ -1,26 +1,34 @@
 import pandas as pd
 
-# df=pd.read_csv("../survey_results_public.csv")
+# person={
+#     "first_name":["tanushree","ashwini","shrishti","somya"],
+#     "second_name":["sinha","menon","sharma","shree"],
+#     "email":["tanushree@gmail.com","ashwini@gmail.com","shrishti@gmail.com","somya@gmail.com"]
+# }
+
+# df=pd.DataFrame(person)         #To convert a dictionary into a data frame
 # print(df)
 
-person={
-    "first":['tanushree','shristi','ashwini','somya'],
-    "last":['sinha','sharma','menon','shree'],
-    "email":['tanushree.sinha@Gmail.com','shristi.sharma@gmail.com','ashwini.menon@gmail.com','somya.shree@gmail.com']
-}
+#EXTRACTION FROM DATA SETS
 
-df=pd.DataFrame(person)
-# print(df)
+# print(df.email)         #To print a series or extra t a series
+# print(df[["email","first_name"]])       #To extract multiple columns
 
-# print(df['email'])
+# print(df.columns)       #To get detailed list of all columns
 
-# print(df[["email","last"]])
+#USING iloc and loc
 
-# print(df.columns)
-
-# print(df.iloc[[1,0,2]])
+# print(df.iloc[0])               #To show the detail of xth row
+# print(df.iloc[[0,1]])           #To show more than one row using there index number
 
 
-# print(df.iloc[0:2])
+# print(df.iloc[[0,1,2],[0,2]])           #To see the speific column detail of that row 
+# You can choose multiple rows and multiple columns
+# You can use [0,1,2] or can directly write 0:3 (without bradckets) df.iloc[0:3,[0,1]]
 
-# print(df.iloc[0:2,0:3])
+#Similarly we can use df.loc , here instead of column index we can use actual name if known
+
+# print(df.loc[[0,1,2],["email"]])
+# print(df.loc[0:4,["email","second_name"]])
+
+
